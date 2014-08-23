7fd4801b38e4098102d601ab08b1a1048808c326
Ruben <yosoyruben@gmail.com>
2014-08-23 14:23:19 +0200

    Trying to do something well creating kv templates

--- gui.py
+++ gui.py
@@ -1,28 +1,34 @@
 import kivy
 kivy.require('1.8.0')
 
-from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.screenmanager import Screen
+from kivy.properties import StringProperty
+from kivy.lang import Builder
+
+from kivy.config import Config
+Config.set('kivy', 'window_icon', 'icon.png')
+Config.set('graphics', 'height', 600)
+Config.set('graphics', 'width', 800)
 
 
-class Controller(FloatLayout):
-    '''Create a controller that receives a custom widget from the kv lang file.
-
-    Add an action to be called from the kv lang file.
-    '''
-    label_wid = ObjectProperty()
+class MainScreen(Screen):
     info = StringProperty()
 
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+
     def do_action(self):
-        self.label_wid.text = 'My label after button press'
         self.info = 'New info text'
 
+    Builder.load_file('views/home.kv')
 
-class ControllerApp(App):
+
+class MainApp(App):
+    title = 'Ouuuuu yeah!'
 
     def build(self):
-        return Controller(info='Hello world')
+        return MainScreen(info='Hello world')
 
 if __name__ == '__main__':
-    ControllerApp().run()
+    MainApp().run()
